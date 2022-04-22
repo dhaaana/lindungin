d940bcb6767f48d5fc1dcf0d422a818ae6c3dcb7
dhaaana <ddhanarr@gmail.com>
2022-04-22 09:09:19 +0700

    add github actions workflows
    add coba column
    
    delete column coba
    
    navbar

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('navigation');
+});
+
+Route::get('/forum', function () {
+    return view('halaman-forum');
 });
