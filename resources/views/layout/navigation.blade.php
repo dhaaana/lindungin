4d2e1c2da02bea990a44d63d1554057e4b396329
Ramadhana Rizqy Arifin <81164825+dhaaana@users.noreply.github.com>
2022-05-23 23:59:51 +0700

    halaman create forum dan halaman update forum (#18)
    Co-authored-by: I Kadek Wahyu Dwi Pranatasana <1415skyblue@gmail.com>
    Co-authored-by: maulanaahmadi <maulanaahmadi352@gmail.com>
    Co-authored-by: Ni Putu Septiary Devi Saraswati <87413208+deviiih@users.noreply.github.com>

--- resources/views/layout/navigation.blade.php
+++ resources/views/layout/navigation.blade.php
@@ -23,6 +23,8 @@
 
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 
     <!-- Font Awesome -->
@@ -36,7 +38,6 @@
     <!-- RichText -->
     <link rel="stylesheet" href="css/richtext.min.css">
     <script src="js/jquery.richtext.min.js"></script>
-
 
 </head>
 
